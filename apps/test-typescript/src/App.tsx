594b33ae25d393c620c0dc3ef65eed5cabb9f283
WillGitCode <wsilburn@ymail.com>
2020-11-08 20:45:45 -0500

    started event handle functions

--- apps/test-typescript/src/App.tsx
+++ apps/test-typescript/src/App.tsx
@@ -22,65 +22,104 @@ const App = () => {
 };
 
 type DisplayScreenProps = {
-  value: string
+  currentValue: string
+  operationString: string
 }
 
 const DisplayScreen = (props: DisplayScreenProps) => {
   return (
     <div className="display-screen">
-      {props.value}
+      <h6 className="op-string">{props.operationString}</h6>
+      <h5 className="current-val">{props.currentValue}</h5>
     </div>
   );
 };
 
 export default App;
 
+type CalcBodyState = {
+  history: Array<CalcBodyState>;
+  display: string;
+  operationString: string;
+  operation: number;
+  error: boolean;
+}
 
-class CalculatorBody extends React.Component {
+class CalculatorBody extends React.Component<{}, CalcBodyState> {
   constructor(props: object) {
     super(props);
     this.state = {
-      display: ""
+      history: [],
+      display: '0',
+      operationString: '',
+      operation: 0,
+      error: false
     };
+  }
+
+  jumpTo(operation: number) {
+    this.setState({
+      display: this.state.history[operation].display,
+      operation: this.state.history[operation].operation,
+      error: this.state.history[operation].error
+    });
+  }
+
+  private handleValueClick = (i: string) => {
+    //const history = this.state.history.slice(0, this.state.operation + 1);
+    this.setState({
+      display: i,
+      operation: this.state.operation + 1
+    })
+  }
+
+  private handleOperationClick = (i: string) => {
+    //const history = this.state.history.slice(0, this.state.operation + 1);
+    this.setState({
+      display: i,
+      operation: this.state.operation + 1
+    })
   }
 
   render(): JSX.Element {
     return (
-      <div className="Body-content">
-        <DisplayScreen value="Value" />
-        <div className="button-row">
-          <button className="calc-button">{"<="}</button>
-          <button className="calc-button">CE</button>
-          <button className="calc-button">C</button>
-          <button className="calc-button">+/-</button>
-          <button className="calc-button">√</button>
-        </div>
-        <div className="button-row">
-          <button className="calc-button">7</button>
-          <button className="calc-button">8</button>
-          <button className="calc-button">9</button>
-          <button className="calc-button">/</button>
-          <button className="calc-button">%</button>
-        </div>
-        <div className="button-row">
-          <button className="calc-button">4</button>
-          <button className="calc-button">5</button>
-          <button className="calc-button">6</button>
-          <button className="calc-button">*</button>
-          <button className="calc-button">1/x{/*1/the numver*/}</button>
-        </div>
-        <div className="button-row">
-          <button className="calc-button">1</button>
-          <button className="calc-button">2</button>
-          <button className="calc-button">3</button>
-          <button className="calc-button">-</button>
-          <button className="calc-button">π</button>
-        </div>
-        <div className="button-row">
-          <button className="calc-button-double-width">0</button>
-          <button className="calc-button">.</button>
-          <button className="calc-button">+</button>
-          <button className="calc-button">=</button>
+      <div className="body-content">
+        <div className="body-column">
+          <DisplayScreen currentValue={this.state.display} operationString={this.state.operationString} />
+          <div className="button-row">
+            <button onClick={() => this.handleOperationClick("<=")} className="calc-button">{"<="}</button>
+            <button onClick={() => this.handleOperationClick("CE")} className="calc-button">CE</button>
+            <button onClick={() => this.handleOperationClick("C")} className="calc-button">C</button>
+            <button onClick={() => this.handleOperationClick("+/-")} className="calc-button">+/-</button>
+            <button onClick={() => this.handleOperationClick("√")} className="calc-button">√</button>
+          </div>
+          <div className="button-row">
+            <button onClick={() => this.handleValueClick("7")} className="calc-button">7</button>
+            <button onClick={() => this.handleValueClick("8")} className="calc-button">8</button>
+            <button onClick={() => this.handleValueClick("9")} className="calc-button">9</button>
+            <button onClick={() => this.handleValueClick("/")} className="calc-button">/</button>
+            <button onClick={() => this.handleValueClick("%")} className="calc-button">%</button>
+          </div>
+          <div className="button-row">
+            <button onClick={() => this.handleValueClick("4")} className="calc-button">4</button>
+            <button onClick={() => this.handleValueClick("5")} className="calc-button">5</button>
+            <button onClick={() => this.handleValueClick("6")} className="calc-button">6</button>
+            <button onClick={() => this.handleValueClick("*")} className="calc-button">*</button>
+            <button onClick={() => this.handleValueClick("1/x")} className="calc-button">1/x{/*1/the numver*/}</button>
+          </div>
+          <div className="button-row">
+            <button onClick={() => this.handleValueClick("1")} className="calc-button">1</button>
+            <button onClick={() => this.handleValueClick("2")} className="calc-button">2</button>
+            <button onClick={() => this.handleValueClick("3")} className="calc-button">3</button>
+            <button onClick={() => this.handleValueClick("-")} className="calc-button">-</button>
+            <button onClick={() => this.handleValueClick("π")} className="calc-button">π</button>
+          </div>
+          <div className="button-row">
+            <button onClick={() => this.handleValueClick("0")} className="calc-button-double-width">0</button>
+            <button onClick={() => this.handleValueClick(".")} className="calc-button">.</button>
+            <button onClick={() => this.handleValueClick("+")} className="calc-button">+</button>
+            <button onClick={() => this.handleOperationClick("=")} className="calc-button">=</button>
+          </div>
         </div>
       </div>
     );

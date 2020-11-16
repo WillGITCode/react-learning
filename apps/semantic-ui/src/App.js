2bab21910de0ce879803ef63c846c7c81f8bd945
WillGitCode <wsilburn@ymail.com>
2020-11-16 08:09:17 -0500

    Playing with constructor, componentDidMount

--- apps/semantic-ui/src/App.js
+++ apps/semantic-ui/src/App.js
@@ -2,31 +2,77 @@ import React from 'react';
 import faker from 'faker';
 import CommentContainer from './components/CommentContainer';
 
-const comments = [
-  {
-    img: faker.image.avatar(),
-    name: faker.name.firstName(),
-    text: faker.lorem.sentence(),
-    date: faker.date.soon().toLocaleDateString()
-  },
-  {
-    img: faker.image.avatar(),
-    name: faker.name.firstName(),
-    text: faker.lorem.sentence(),
-    date: faker.date.soon().toLocaleDateString()
-  },
-  {
-    img: faker.image.avatar(),
-    name: faker.name.firstName(),
-    text: faker.lorem.sentence(),
-    date: faker.date.soon().toLocaleDateString()
-  }
-];
 
-const App = () => {
-  return (
-    <CommentContainer comments={comments} />
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: [],
+      loading: true,
+      error: ''
+    };
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  getLocation = () => {
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log(position);
+        this.getComments();
+      },
+      e => {
+        console.log(e);
+        this.setState({
+          error: e.message,
+          loading: false
+        })
+      }
+    );
+  }
+
+  getComments = () => {
+    this.setState({
+      loading: false,
+      comments: [
+        {
+          img: faker.image.avatar(),
+          name: faker.name.firstName(),
+          text: faker.lorem.sentence(),
+          date: faker.date.soon().toLocaleDateString()
+        },
+        {
+          img: faker.image.avatar(),
+          name: faker.name.firstName(),
+          text: faker.lorem.sentence(),
+          date: faker.date.soon().toLocaleDateString()
+        },
+        {
+          img: faker.image.avatar(),
+          name: faker.name.firstName(),
+          text: faker.lorem.sentence(),
+          date: faker.date.soon().toLocaleDateString()
+        }
+      ]
+    });
+  }
+
+
+  render() {
+    if (this.state.error && this.state.error.length > 0) {
+      return (
+        <h1>Error: {this.state.error}</h1>
+      );
+    }
+    else if (this.state.comments && this.state.comments.length > 0) {
+      return (
+        <CommentContainer comments={this.state.comments} />
+      );
+    }
+    return <h2>Loading</h2>
+  }
 };
 
 export default App;

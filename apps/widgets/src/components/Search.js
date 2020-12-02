d901860297dce5ad363c6f756239eead114b07e3
WillGitCode <wsilburn@ymail.com>
2020-12-02 09:41:34 -0500

    wikipedia api

--- apps/widgets/src/components/Search.js
+++ apps/widgets/src/components/Search.js
@@ -1,12 +1,41 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
-    console.log('I only run once');
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term
+        }
+      });
+      setResults(data.query.search);
+    }
+    if (term) {
+      search();
+    }
+
   }, [term]);
 
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={results.pageid} className="item">
+        <div className="content">
+          <div className="header">
+            {results.title}
+          </div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -19,6 +48,9 @@ const Search = () => {
             onChange={e => setTerm(e.target.value)}
           />
         </div>
+      </div>
+      <div className="ui celled list">
+        {renderedResults}
       </div>
     </div>
   );
